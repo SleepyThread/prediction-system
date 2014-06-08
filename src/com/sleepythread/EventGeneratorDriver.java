130753818294cd48a2b20b63346793422a4c056e
Akash <akash.mishra20@gmail.com>
2014-06-08 19:20:16 +0530

    Added Prediction Generation Map Reduce

---
+++ src/com/sleepythread/EventGeneratorDriver.java
@@ -0,0 +1,44 @@
+package com.sleepythread;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class EventGeneratorDriver extends Configured implements Tool {
+
+    @Override
+    public int run(String[] args) throws Exception {
+
+        Configuration configuration = getConf();
+
+        configuration.set("startDate", "2014-01-01");
+        configuration.set("endDate", "2015-01-01");
+
+        Job job = new Job(configuration, "Event Generator");
+
+        job.setJarByClass(EventGeneratorDriver.class);
+        FileInputFormat.setInputPaths(job, new Path(System.getProperty("inputPath")));
+        FileOutputFormat.setOutputPath(job, new Path(System.getProperty("outputPath")));
+
+        job.setMapperClass(EventGeneratorMapper.class);
+        job.setMapOutputKeyClass(NullWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setNumReduceTasks(0);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new Configuration(),
+                new EventGeneratorDriver(), args);
+        System.exit(exitCode);
+    }
+}
---
+++ src/com/sleepythread/EventGeneratorMapper.java
@@ -0,0 +1,52 @@
+package com.sleepythread;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+
+public class EventGeneratorMapper extends Mapper<Object, Text, NullWritable, Text> {
+
+    public static final String HIVE_COLUMN_SEPARATOR = "\u0001";
+
+    @Override
+    public void map(Object key, Text record, Context context) throws IOException, InterruptedException {
+        Configuration configuration = context.getConfiguration();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+        DateTime endDate = DateTime.parse(configuration.get("endDate"),dateTimeFormatter);
+
+        String[] split = record.toString().split(HIVE_COLUMN_SEPARATOR);
+
+        Double id = Double.parseDouble(split[0]);
+        Double interval = Double.parseDouble(split[1]);
+        Double rate = Double.parseDouble(split[2]);
+        Double current_usage = Double.parseDouble(split[3]);
+        DateTime capture_date = DateTime.parse(split[4],dateTimeFormatter);
+
+        Double nextServicingKm = Math.ceil(current_usage / interval)*rate;
+        Double noOfDaysToReachNextServingKm = (nextServicingKm - current_usage)/rate;
+        DateTime nextServicingDate = capture_date.plusDays(noOfDaysToReachNextServingKm.intValue());
+
+        Double daysToCompleteIntervalUsingRate = (interval/rate);
+
+
+        while (nextServicingDate.isBefore(endDate)){
+            Text text = new Text(id+HIVE_COLUMN_SEPARATOR+nextServicingKm+HIVE_COLUMN_SEPARATOR+nextServicingKm);
+            context.write(NullWritable.get(), text);
+
+            nextServicingKm = nextServicingKm + interval;
+            nextServicingDate = nextServicingDate.plusDays(daysToCompleteIntervalUsingRate.intValue());
+        }
+    }
+
+}
